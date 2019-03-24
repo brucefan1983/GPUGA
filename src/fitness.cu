a7da2aa7c98dd2d4b5bb508e7168aa391b3c27cd
Zheyong Fan <brucenju@gmail.com>
2019-03-24 18:33:38 +0200

    start to add force error in the fitness function

--- src/fitness.cu
+++ src/fitness.cu
@@ -31,6 +31,12 @@ Fitness::Fitness(void)
 }
 
 
+Fitness::~Fitness(void)
+{
+    // nothing now
+}
+
+
 void Fitness::compute
 (
     int population_size, int number_of_variables, 
@@ -52,9 +58,80 @@ void Fitness::compute
 }
 
 
-Fitness::~Fitness(void)
+
+void Fitness::get_fitness_population
+(
+    int population_size, int number_of_variables, 
+    double* population, double* fitness
+)
 {
-    // nothing now
+    for (int n = 0; n < population_size; ++n)
+    {
+        double* individual = population + n * number_of_variables;
+        for (int m = 0; m < number_of_variables; ++m)
+        {
+            double a = potential_parameters_min[m];
+            double b = potential_parameters_max[m] - a;
+            potential_parameters[m] = a + b * individual[m];
+        }
+        find_force();
+        fitness[n] = get_fitness_force();
+    }
+}
+
+
+static __device__ void warp_reduce(volatile double* s, int t) 
+{
+    s[t] += s[t + 32]; s[t] += s[t + 16]; s[t] += s[t + 8];
+    s[t] += s[t + 4];  s[t] += s[t + 2];  s[t] += s[t + 1];
+}
+
+void Fitness::find_force(void)
+{
+    // TODO
+}
+
+
+static __global__ void gpu_sum_force_error
+(
+    int N, double *g_fx, double *g_fy, double *g_fz, 
+    double *g_fx_ref, double *g_fy_ref, double *g_fz_ref, double *g_error
+)
+{
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+    int number_of_patches = (N - 1) / 1024 + 1; 
+    __shared__ double s_error[1024];
+    s_error[tid] = 0.0;
+    for (int patch = 0; patch < number_of_patches; ++patch)
+    {
+        int n = tid + patch * 1024;
+        if (n < N) 
+        {
+            double dx = g_fx[n] - g_fx_ref[n];
+            double dy = g_fy[n] - g_fy_ref[n];
+            double dz = g_fz[n] - g_fz_ref[n];
+            s_error[tid] += dx * dx + dy * dy + dz * dz;
+        }
+    }
+    __syncthreads();
+    if (tid < 512) s_error[tid] += s_error[tid + 512]; __syncthreads();
+    if (tid < 256) s_error[tid] += s_error[tid + 256]; __syncthreads();
+    if (tid < 128) s_error[tid] += s_error[tid + 128]; __syncthreads();
+    if (tid <  64) s_error[tid] += s_error[tid + 64];  __syncthreads();
+    if (tid <  32) warp_reduce(s_error, tid);
+    if (tid ==  0) { g_error[bid] = s_error[0]; }
+}
+
+
+double Fitness::get_fitness_force(void)
+{
+    gpu_sum_force_error<<<1, 1024>>>(N, fx, fy, fz, 
+        fx_ref, fy_ref, fz_ref, force_error_gpu);
+    CHECK(cudaMemcpy(force_error_cpu, force_error_gpu, sizeof(double), 
+        cudaMemcpyDeviceToHost));
+    force_error_cpu[0] /= force_ref_square_sum;
+    return force_error_cpu[0];
 }
 
 
--- src/fitness.cuh
+++ src/fitness.cuh
@@ -24,7 +24,20 @@ public:
     ~Fitness(void);
     void compute(int, int, double*, double*);
 protected:
-    // nothing now
+    // functions
+    void get_fitness_population(int, int, double*, double*);
+    double get_fitness_force(void);
+    void find_force(void);
+
+    // variables
+    double* potential_parameters;
+    double* potential_parameters_min;
+    double* potential_parameters_max;
+    int N; // number of atoms
+    double *fx, *fy, *fz;
+    double *fx_ref, *fy_ref, *fz_ref; 
+    double *force_error_gpu, *force_error_cpu;
+    double force_ref_square_sum;
 };
 
 
