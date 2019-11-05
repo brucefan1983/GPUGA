26086a15e74d40a960a39d915a1e26524aefd786
Zheyong Fan <brucenju@gmail.com>
2019-11-05 16:35:23 +0200

    new output format

--- examples/si_diamond/plot_results.m
+++ examples/si_diamond/plot_results.m
@@ -1,27 +1,31 @@
-clear;close all;
-load a.txt; % DFT input
-load prediction.out; % GPUGA output
-load force.out; % GPUGA output
+clear; close all;
 
-prediction=prediction/64;
-N=53;
-M=4;
-energy=prediction(M+1:N*3+M,:);
-virial=prediction(N*3+M+1:end,:);
+load a.txt; % lattice constant (Angstrom)
+N=size(a,1); % number of configurations for one deformation type
 
-df=force(:,1:3)-force(:,4:6);
-df=reshape(df,960,1);
-f=reshape(force(:,4:6),960,1);
-error_f=std(df,1)/std(f,1)
-de=prediction(5:end/4,1)-prediction(5:end/4,2);
-e=prediction(5:end/4,2);
-error_e=std(de,1)/std(e,1)
-dv=prediction(end/4+1:end,1)-prediction(end/4+1:end,2);
-v=prediction(end/4+1:end,2);
-error_v=std(dv,1)/std(v,1)
+% get the outputs from GPUGA:
+load energy.out;
+load virial.out;
+load force.out;
 
-figure;
+% normalized by the number of atoms
+num_atoms=64; % I used 64 atoms in all the deformed configurations
+energy=energy/num_atoms; % eV/atom
+virial=virial/num_atoms; % eV/atom
 
+% exclude a few polytypes in the figure
+M=4; % number of configurations for some polytypes
+energy=energy(M+1:end,:);
+virial_xx=virial(0*end/6+M+1:1*end/6,:);
+virial_yy=virial(1*end/6+M+1:2*end/6,:);
+virial_zz=virial(2*end/6+M+1:3*end/6,:);
+virial_xy=virial(3*end/6+M+1:4*end/6,:);
+virial_yz=virial(4*end/6+M+1:5*end/6,:);
+virial_zx=virial(5*end/6+M+1:6*end/6,:);
+
+figure; % Figure 2 in the paper
+
+% panel (a): force
 subplot(1,3,1)
 plot(force(1:end,4:6),force(1:end,1:3),'o');
 xlim([-2,2]);
@@ -31,20 +35,15 @@ ylabel('Force (Potential) (eV/\AA)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 title('(a)');
 
+% panel (b): energy
 subplot(1,3,2);
-n=1;
-index=(n-1)*N+1:n*N;
-plot(a,energy(index,2),'d');
+plot(a,energy(1:end/3,2),'d');
 hold on;
-plot(a,energy(index,1),'-','linewidth',2);
-n=2;
-index=(n-1)*N+1:n*N;
-plot(a,energy(index,2),'s');
-plot(a,energy(index,1),'--','linewidth',2);
-n=3;
-index=(n-1)*N+1:n*N;
-plot(a,energy(index,2),'o');
-plot(a,energy(index,1),'-.','linewidth',2);
+plot(a,energy(1:end/3,1),'-','linewidth',2);
+plot(a,energy(end/3+1:2*end/3,2),'s');
+plot(a,energy(end/3+1:2*end/3,1),'--','linewidth',2);
+plot(a,energy(2*end/3+1:3*end/3,2),'o');
+plot(a,energy(2*end/3+1:3*end/3,1),'-.','linewidth',2);
 axis tight
 xlabel('$a$ (\AA)','fontsize',12,'interpreter','latex');
 ylabel('Energy (eV)','fontsize',12,'interpreter','latex');
@@ -53,16 +52,19 @@ legend('triaxial-DFT','triaxial-Potential','biaxial-DFT',...
    'biaxial-Potential','uniaxial-DFT','uniaxial-Potential');
 title('(b)');
 
+% panel (c): virial in a deformed direction
 subplot(1,3,3);
-plot(a,virial(M+1:N+M,2),'d','linewidth',1);
+plot(a,virial_xx(1:end/3,2),'d','linewidth',1);
 hold on;
-plot(a,virial(M+1:N+M,1),'-','linewidth',2);
-plot(a,virial(N+M+1:2*N+M,2),'d','linewidth',1);
-plot(a,virial(N+M+1:2*N+M,1),'-','linewidth',2);
-plot(a,virial(end-N+1:end,2),'s','linewidth',1);
-plot(a,virial(end-N+1:end,1),'--','linewidth',2);
+plot(a,virial_xx(1:end/3,1),'-','linewidth',2);
+plot(a,virial_xx(end/3+1:2*end/3,2),'d','linewidth',1);
+plot(a,virial_xx(end/3+1:2*end/3,1),'-','linewidth',2);
+plot(a,virial_zz(2*end/3+1:end,2),'s','linewidth',1);
+plot(a,virial_zz(2*end/3+1:end,1),'--','linewidth',2);
 axis tight
 xlabel('$a$ (\AA)','fontsize',12,'interpreter','latex');
 ylabel('Virial (eV)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 title('(c)');
+
+
